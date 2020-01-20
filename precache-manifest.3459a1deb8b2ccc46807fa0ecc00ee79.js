d3bc4ee77d9fb77051a9f43c7d1b03eb92063359
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-01-19 17:47:12 -0800

    Updates

---
+++ precache-manifest.3459a1deb8b2ccc46807fa0ecc00ee79.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d5c0a1eaad52fd2c82348b028e7bc5",
+    "url": "/spacex-client-build/index.html"
+  },
+  {
+    "revision": "128e8dac1ce5b0684739",
+    "url": "/spacex-client-build/static/js/2.c1cde0ba.chunk.js"
+  },
+  {
+    "revision": "e4b1dff13a0dcba2ee3c",
+    "url": "/spacex-client-build/static/js/main.921bfd09.chunk.js"
+  },
+  {
+    "revision": "9677f5233b99b8e4744f",
+    "url": "/spacex-client-build/static/js/runtime~main.eb586662.js"
+  },
+  {
+    "revision": "0a4ee70bfcddd57e81c1c5280aa73122",
+    "url": "/spacex-client-build/static/media/cart.0a4ee70b.svg"
+  },
+  {
+    "revision": "33d00d4051b4139369bdfce6c6ad4ebb",
+    "url": "/spacex-client-build/static/media/curve.33d00d40.svg"
+  },
+  {
+    "revision": "f25b4cba48a496093085f268698a4b39",
+    "url": "/spacex-client-build/static/media/dog-1.f25b4cba.png"
+  },
+  {
+    "revision": "ab7a533bb445ea0d167a34cdc0eec387",
+    "url": "/spacex-client-build/static/media/dog-2.ab7a533b.png"
+  },
+  {
+    "revision": "b62803749fd80890a425b0dfae080c47",
+    "url": "/spacex-client-build/static/media/dog-3.b6280374.png"
+  },
+  {
+    "revision": "5bb388b5d1c87d4b12dff9a89ab75766",
+    "url": "/spacex-client-build/static/media/exit.5bb388b5.svg"
+  },
+  {
+    "revision": "0b1c67758d564d5eb140740847e6886e",
+    "url": "/spacex-client-build/static/media/galaxy.0b1c6775.jpg"
+  },
+  {
+    "revision": "a8474d8eef7ab919939d844420c89b6e",
+    "url": "/spacex-client-build/static/media/home.a8474d8e.svg"
+  },
+  {
+    "revision": "48932214f494cf346794f464ee2c04e3",
+    "url": "/spacex-client-build/static/media/iss.48932214.jpg"
+  },
+  {
+    "revision": "3e5f8e367babc8286909705d3f93c10d",
+    "url": "/spacex-client-build/static/media/logo.3e5f8e36.svg"
+  },
+  {
+    "revision": "493bbb752333aacb0e80bab31c2e925a",
+    "url": "/spacex-client-build/static/media/moon.493bbb75.jpg"
+  },
+  {
+    "revision": "3eddd92283b12f509d52858bbf7fbf60",
+    "url": "/spacex-client-build/static/media/profile.3eddd922.svg"
+  },
+  {
+    "revision": "aeabe03413a40651e399da5c465937eb",
+    "url": "/spacex-client-build/static/media/rocket.aeabe034.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/spacex-client-build/static/media/space.7dfc7923.jpg"
+  }
+]);
